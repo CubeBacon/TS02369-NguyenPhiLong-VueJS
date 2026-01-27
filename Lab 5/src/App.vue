5178e25fb0cd20e19e438d992cda899f3f400567
CubeBacon <lolti111isbacon@gmail.com>
2026-01-27 12:23:57 +0700

    Nop bai 4 cua lab 5

--- Lab 5/src/App.vue
+++ Lab 5/src/App.vue
@@ -1,10 +1,27 @@
-<script setup>
-import Bai3 from './components/Bai3.vue'
-</script>
-
 <template>
-  <Bai3 />
+  <div id="app">
+    <h1>Ứng dụng Blog nhỏ với Vue.js</h1>
+    <CreatePost @add-post="addPost" />
+    <PostList :posts="posts" />
+  </div>
 </template>
 
-<style scoped>
+<script setup>
+import { ref } from 'vue'
+import CreatePost from './components/Bai4/CreatePost.vue'
+import PostList from './components/Bai4/PostList.vue'
+
+const posts = ref([])
+
+function addPost(post) {
+  posts.value.push(post)
+}
+</script>
+
+<style>
+#app {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
 </style>
